860ddb3ee804a1f24c58c10f6018afaf697b529b
abdullahmohsen <44713320+abdullahmohsen@users.noreply.github.com>
2020-10-22 21:14:18 +0200

    Done Actors

--- app/ViewModels/MovieViewModel.php
+++ app/ViewModels/MovieViewModel.php
@@ -19,7 +19,7 @@ class MovieViewModel extends ViewModel
         return collect($this->movie)->merge([
             'poster_path' => $this->movie['poster_path']
                 ? 'https://image.tmdb.org/t/p/w500/'.$this->movie['poster_path']
-                : 'https://via.placeholder.com/500x750',
+                : 'https://via.placeholder.com/500x750?text=No+Photo',
             'vote_average' => $this->movie['vote_average'] * 10 .'%',
             'release_date' => Carbon::parse($this->movie['release_date'])->format('M d, Y'),
             'genres' => collect($this->movie['genres'])->pluck('name')->flatten()->implode(', '),
@@ -28,7 +28,8 @@ class MovieViewModel extends ViewModel
                 return collect($cast)->merge([
                     'profile_path' => $cast['profile_path']
                         ? 'https://image.tmdb.org/t/p/w300'.$cast['profile_path']
-                        : 'https://via.placeholder.com/300x450',
+                        : 'https://via.placeholder.com/300x450?text=No+Photo'
+                        // :'https://ui-avatars.com/api/?size=300x450&name='.$cast['name']
                 ]);
             }),
             'images' => collect($this->movie['images']['backdrops'])->take(9),
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
-use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
-
-
-
+use App\Http\Controllers\ActorsController;
+use App\Http\Controllers\MoviesController;
 
 
 Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
-Route::get('/movie/{id}', [MoviesController::class, 'show'])->name('movies.show');
+Route::get('/movie/{movie}', [MoviesController::class, 'show'])->name('movies.show');
+
+
+
+
+
+Route::get('/actors', [ActorsController::class, 'index'])->name('actors.index');
+Route::get('/actors/page/{page?}', [ActorsController::class, 'index']);
+Route::get('/actors/{id}', [ActorsController::class, 'show'])->name('actors.show');
+
